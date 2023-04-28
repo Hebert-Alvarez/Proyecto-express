9be67bd1fccccf74c9b71534628812270560d9ef
Hebert-Alvarez <hebertalvarez52@hotmail.com>
2023-04-28 09:40:40 -0600

    28/04/2023 - manejo de rutas

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', function(req, res){
     res.send('Una API basica desde Express');
 });
 
+app.get('/saludo', function(req, res){
+    res.send('Hola desde la API');
+});
+
+app.get('/despedida', function(req, res){
+    res.send('Adios desde la API');
+});
+
 app.listen(3000, function(){
     console.log('Aplicación ejemplo, escuchando el puerto 3000!');
 });
